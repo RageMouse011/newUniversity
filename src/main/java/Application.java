17e2f265e8788a2e8e7996b024cbd9eacfbc8987
Daniil <zinindaniil11@gmail.com>
2022-11-16 16:05:16 +0600

    Fird commit. (getStudentById, getAllStudents methods were added. Plus a little bit stylistic changes.)

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,7 +1,7 @@
+import entities.Student;
 import sql.PaymentSQL;
 import sql.StudentSQL;
 
-import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -12,77 +12,102 @@ public class Application {
         StudentSQL studentSQL = new StudentSQL();
         PaymentSQL paymentSQL = new PaymentSQL();
 
-        System.out.println("Enter the appropriate number for further action: " +
-                "\n 1: Create student. \n 2: Update student. \n 3. Delete student." +
-                "\n 4. Create payment. \n 5. Update payment. \n 6. Retrieve student's payments." +
-                "\n 7. Retrieve all student's payments.");
+        while (true) {
+            System.out.println("""
+                    Enter the appropriate number for further action:\s
+                     1: Create student.\s
+                     2: Update student.\s
+                     3. Delete student.
+                     4. Create payment.\s
+                     5. Update payment.\s
+                     6. Retrieve student's payments.
+                     7. Retrieve all student's payments.\s
+                     8. Retrieve specific student.\s
+                     9. Retrieve all students.\s
+                     10. Choose to quit the program""");
 
-        Scanner input = new Scanner(System.in);
-        int decision = input.nextInt();
+            Scanner input = new Scanner(System.in);
+            int decision = input.nextInt();
 
-        switch (decision) {
-            case 1:
-                System.out.println("Enter the firstname, lastname and faculty (after each field click enter): ");
-                List<String> case1 = new ArrayList<>();
-                Scanner getCase1 = new Scanner(System.in);
-                boolean quitCase1 = false;
-                while(!quitCase1) {
-                    System.out.println("Enter q to quit.");
-                    String inputCase1 = getCase1.nextLine();
-                    if(inputCase1.equals("q")) {
-                        quitCase1 = true;
-                    } else {
-                        case1.add(inputCase1);
+            switch (decision) {
+                case 1:
+                    System.out.println("Enter the firstname, lastname and faculty (after each field click enter): ");
+                    List<String> case1 = new ArrayList<>();
+                    Scanner getCase1 = new Scanner(System.in);
+                    boolean quitCase1 = false;
+
+                    Student student = new Student();
+                    while (!quitCase1) {
+                        System.out.println("Enter q to quit.");
+                        String inputCase1 = getCase1.nextLine();
+                        if (inputCase1.equals("q")) {
+                            quitCase1 = true;
+                        } else {
+                            case1.add(inputCase1);
+                        }
                     }
-                }
-                studentSQL.create(case1.get(0), case1.get(1), case1.get(2));
+                    student.setFirstName(case1.get(0));
+                    student.setLastName(case1.get(1));
+                    student.setFaculty(case1.get(2));
 
-
-            break;
-            case 2:
-                System.out.println("Enter new faculty: ");
-                Scanner getCase2 = new Scanner(System.in);
-                String inputCase2 = getCase2.next();
-                studentSQL.update(inputCase2);
-            break;
-            case 3:
-                System.out.println("Enter the id of student which you want to remove: ");
-                Scanner getCase3 = new Scanner(System.in);
-                int inputCase3 = getCase3.nextInt();
-                studentSQL.delete(inputCase3);
-            break;
-            case 4:
-                System.out.println("Enter the title, price and student id: ");
-                Scanner getCase4 = new Scanner(System.in);
-                String inputCase41 = getCase4.nextLine();
-                int inputCase42 = getCase4.nextInt();
-                int inputCase43 = getCase4.nextInt();
-                paymentSQL.create(inputCase41, inputCase42, inputCase43);
-            break;
-            case 5:
-                System.out.println("Enter the price and id of the payment: ");
-                List<Integer> case5 = new ArrayList<>();
-                Scanner getCase5 = new Scanner(System.in);
-                boolean quitCase2 = false;
-                while(!quitCase2) {
-                    System.out.println("Enter 0 to quit.");
-                    Integer inputCase5 = getCase5.nextInt();
-                    if(inputCase5.equals(0)) {
-                        quitCase2 = true;
-                    } else {
-                        case5.add(inputCase5);
+                    studentSQL.create(student);
+                    break;
+                case 2:
+                    System.out.println("Enter new faculty: ");
+                    Scanner getCase2 = new Scanner(System.in);
+                    String inputCase2 = getCase2.next();
+                    studentSQL.update(inputCase2);
+                    break;
+                case 3:
+                    System.out.println("Enter the id of student which you want to remove: ");
+                    Scanner getCase3 = new Scanner(System.in);
+                    int inputCase3 = getCase3.nextInt();
+                    studentSQL.delete(inputCase3);
+                    break;
+                case 4:
+                    System.out.println("Enter the title, price and student id: ");
+                    Scanner getCase4 = new Scanner(System.in);
+                    String inputCase41 = getCase4.nextLine();
+                    int inputCase42 = getCase4.nextInt();
+                    int inputCase43 = getCase4.nextInt();
+                    paymentSQL.create(inputCase41, inputCase42, inputCase43);
+                    break;
+                case 5:
+                    System.out.println("Enter the price and id of the payment: ");
+                    List<Integer> case5 = new ArrayList<>();
+                    Scanner getCase5 = new Scanner(System.in);
+                    boolean quitCase2 = false;
+                    while (!quitCase2) {
+                        System.out.println("Enter 0 to quit.");
+                        Integer inputCase5 = getCase5.nextInt();
+                        if (inputCase5.equals(0)) {
+                            quitCase2 = true;
+                        } else {
+                            case5.add(inputCase5);
+                        }
                     }
-                }
-                paymentSQL.update(case5.get(0), case5.get(1));
-            break;
-            case 6:
-                System.out.println("Enter the student id: ");
-                Scanner getCase6 = new Scanner(System.in);
-                int inputCase6 = getCase6.nextInt();
-                paymentSQL.retrieveStudentsPayments(inputCase6);
-            break;
-            case 7:
-                paymentSQL.retrieveAllStudentsPayments();
+                    paymentSQL.update(case5.get(0), case5.get(1));
+                    break;
+                case 6:
+                    System.out.println("Enter the student id: ");
+                    Scanner getCase6 = new Scanner(System.in);
+                    int inputCase6 = getCase6.nextInt();
+                    paymentSQL.retrieveStudentsPayments(inputCase6);
+                    break;
+                case 7:
+                    paymentSQL.retrieveAllStudentsPayments();
+                    break;
+                case 8:
+                    System.out.println("Enter the student id: ");
+                    Scanner getCase8 = new Scanner(System.in);
+                    int inputCase8 = getCase8.nextInt();
+                    studentSQL.getStudentById(inputCase8);
+                    break;
+                case 9:
+                    studentSQL.getAllStudents();
+                case 10:
+                    return;
+            }
         }
 
 
--- src/main/java/sql/StudentSQL.java
+++ src/main/java/sql/StudentSQL.java
@@ -1,6 +1,10 @@
 package sql;
 
+import entities.Student;
+
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class StudentSQL {
     Connection conn = null;
@@ -8,30 +12,27 @@ public class StudentSQL {
     String user = "postgres";
     String password = "Q12we34r56t";
 
-    public boolean create(String firstName, String lastName, String faculty) {
+    public boolean create(Student student) {
         boolean result = false;
         String create = "insert into student (first_name, last_name, faculty) values (?, ?, ?)";
         try {
             Class.forName("org.postgresql.Driver");
             conn = DriverManager.getConnection(url, user, password);
             PreparedStatement ps = conn.prepareStatement(create);
-            ps.setString(1, firstName);
-            ps.setString(2, lastName);
-            ps.setString(3, faculty);
+            ps.setString(1, student.getFirstName());
+            ps.setString(2, student.getLastName());
+            ps.setString(3, student.getFaculty());
 
-            ps.execute();
-            result = true;
+            result = ps.execute();
 
             ps.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-            result = false;
-        } catch (ClassNotFoundException e) {
+        } catch (SQLException | ClassNotFoundException e) {
             e.printStackTrace();
         } finally {
             connClose();
-            return result;
         }
+        System.out.println(result);
+        return result;
     }
 
     public boolean update(String faculty) {
@@ -44,21 +45,15 @@ public class StudentSQL {
             ps.setString(1, faculty);
             ps.setInt(2, 23);
 
-            ps.execute();
-            result = true;
-
+            result = ps.execute();
 
             ps.close();
-        } catch (SQLException e) {
+        } catch (SQLException | ClassNotFoundException e) {
             e.printStackTrace();
-
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-            result = false;
         } finally {
             connClose();
-            return result;
         }
+        return result;
     }
 
     public boolean delete(int inputCase3) {
@@ -74,21 +69,71 @@ public class StudentSQL {
             ps = conn.prepareStatement(delete);
             ps.setInt(1, inputCase3);
 
-            ps.execute();
-            result = true;
-
+            result = ps.execute();
 
             ps.close();
-        } catch (SQLException e) {
+        } catch (SQLException | ClassNotFoundException e) {
             e.printStackTrace();
-
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-            result = false;
         } finally {
             connClose();
-            return result;
         }
+        return result;
+    }
+
+    public Student getStudentById(int inputCase8) {
+        String getStudentById = "select first_name, last_name, faculty from student where id =?";
+        Student student = new Student();
+        try {
+            Class.forName("org.postgresql.Driver");
+            conn = DriverManager.getConnection(url, user, password);
+            PreparedStatement ps = conn.prepareStatement(getStudentById);
+            ps.setInt(1, inputCase8);
+
+            ResultSet rs = ps.executeQuery();
+
+            while(rs.next()) {
+                student.setFirstName(rs.getString("first_name"));
+                student.setLastName(rs.getString("last_name"));
+                student.setFaculty(rs.getString("faculty"));
+            }
+
+            ps.close();
+            rs.close();
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            connClose();
+        }
+        System.out.println(student);
+        return student;
+    }
+
+    public List<Student> getAllStudents() {
+        List<Student> result = new ArrayList<>();
+        String getAllStudents = "select first_name, last_name, faculty from student";
+        try {
+            Class.forName("org.postgresql.Driver");
+            conn = DriverManager.getConnection(url, user, password);
+            PreparedStatement ps = conn.prepareStatement(getAllStudents);
+
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()) {
+                Student student = new Student();
+                student.setFirstName(rs.getString("first_name"));
+                student.setLastName(rs.getString("last_name"));
+                student.setFaculty(rs.getString("faculty"));
+                result.add(student);
+            }
+
+            ps.close();
+            rs.close();
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            connClose();
+        }
+        System.out.println(result);
+        return result;
     }
 
     public void connClose() {
